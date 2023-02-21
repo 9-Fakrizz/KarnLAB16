16738ac7812434ff9f188532be71fe747a799236
suphakrit <suphakrit_kongkham@cmu.ac.th>
2023-02-21 14:05:17 +0700

    H_H

--- lab16_1.cpp
+++ lab16_1.cpp
@@ -1,7 +1,59 @@
-include <iostream>
+#include <iostream>
 using namespace std;
 
 int main(){
+	int a = 5;
+	string b = "A";
+	string &c = b;
+	int *x = &a;
+	string *y = &b;
+	int *z = x;
+
+	cout << a <<" "; 
+	cout << b <<" "; 
+	cout << c <<" "; 
+	cout << x <<" "; 
+	cout << y <<" "; 
+	cout << z <<"\n"; 
+
+	cout << &a <<" "; 
+	cout << &b <<" "; 
+	cout << &c <<" "; 
+	cout << &x <<" "; 
+	cout << &y <<" "; 
+	cout << &z <<"\n"; 
+
+	c = "F";
+	cout << a <<" "; 
+	cout << b <<" "; 
+	cout << c <<" "; 
+	cout << x <<" "; 
+	cout << y <<" "; 
+	cout << z <<"\n"; 
+
+	*y = "F";
+	cout << a <<" "; 
+	cout << b <<" "; 
+	cout << c <<" "; 
+	cout << x <<" "; 
+	cout << y <<" "; 
+	cout << z <<"\n"; 
+
+	*x = 6;
+	cout << a <<" "; 
+	cout << b <<" "; 
+	cout << c <<" "; 
+	cout << x <<" "; 
+	cout << y <<" "; 
+	cout << z <<"\n"; 
+
+	*z=7;
+	cout << a <<" "; 
+	cout << b <<" "; 
+	cout << c <<" "; 
+	cout << x <<" "; 
+	cout << y <<" "; 
+	cout << z <<"\n"; 
 
 	
 	
